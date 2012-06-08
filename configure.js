eda16c9db9b5aee8e92396c169c98b8435530cab
hogelog <hogelog@hogel.org>
2012-06-09 04:24:05 +0900

    change configuration method

--- configure.js
+++ configure.js
@@ -8,33 +8,35 @@ jsonconfig.load(["./gyazo.conf"], function(err){
   if (err) throw err;
 });
 
-var HOST = jsonconfig["host"];
+var HOST = "\"" + jsonconfig["host"] + "\"";
 var PORT = jsonconfig["port"];
-var PATH = jsonconfig["path"];
+var PATH = "\"" + jsonconfig["path"] + "\"";
 
-var gyazowin_header =
-  "#define GYAZO_HOST \"" + HOST + "\"\n" +
-  "#define GYAZO_PORT " + PORT + "\n" +
-  "#define GYAZO_PATH \"" + PATH + "\"\n";
+function configure(src, dst, msg) {
+  fs.readFile(src, "utf-8", function(err, data){
+    if (err) throw err;
+    var script = data
+      .replace("--HOST--", HOST)
+      .replace("--PORT--", PORT)
+      .replace("--PATH--", PATH);
+    fs.writeFile(dst, script, function(err){
+      if (err) throw err;
+      console.log(msg);
+    });
+  });
+}
 
-fs.writeFile("client/Gyazowin/gyazowin/config.h", gyazowin_header, function(err){
-  if (err) throw err;
-  console.log("configure client/Gyazowin");
-});
+configure(
+  "client/Gyazowin/gyazowin/config.source.h",
+  "client/Gyazowin/gyazowin/config.h",
+  "configure client/Gyazowin");
 
-var ruby_config = 
-  "class GyazoConfig\n" +
-    "def self.host;\"" + HOST + "\";end\n" +
-    "def self.port;" + PORT + ";end\n" +
-    "def self.PATH;\"" + PATH + "\";end\n" +
-  "end";
+configure(
+  "client/Gyazo-for-Linux/gyazo.source",
+  "client/Gyazo-for-Linux/gyazo",
+  "configure client/Gyazo-for-Linux");
 
-fs.writeFile("client/Gyazo-for-Linux/config.rb", ruby_config, function(err){
-  if (err) throw err;
-  console.log("configure client/Gyazo-for-Linux");
-});
-
-fs.writeFile("client/Gyazo/Gyazo.app/Contents/Resources/config.rb", ruby_config, function(err){
-  if (err) throw err;
-  console.log("configure client/Gyazo");
-});
+configure(
+  "client/Gyazo/Gyazo.app/Contents/Resources/script.source",
+  "client/Gyazo/Gyazo.app/Contents/Resources/script",
+  "configure client/Gyazo");
