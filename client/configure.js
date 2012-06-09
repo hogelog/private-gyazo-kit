2fd00d327434875675429d5d89ac0da3557eaa4d
hogelog <hogelog@hogel.org>
2012-06-09 15:12:29 +0900

    enable build Windows client without VC and Windows.

--- client/configure.js
+++ client/configure.js
@@ -4,32 +4,27 @@ var
   path = require("path"),
   jsonconfig = require("jsonconfig");
 
-jsonconfig.load(["./gyazo.conf"], function(err){
+jsonconfig.load(["../gyazo.conf"], function(err){
   if (err) throw err;
 });
 
-var HOST = "\"" + jsonconfig["host"] + "\"";
+var HOST = jsonconfig["host"];
 var PORT = jsonconfig["port"];
-var PATH = "\"" + jsonconfig["path"] + "\"";
+var PATH = jsonconfig["path"];
 
 function configure(src, dst, msg) {
   fs.readFile(src, "utf-8", function(err, data){
     if (err) throw err;
     var script = data
-      .replace("--HOST--", HOST)
+      .replace("--HOST--", "\"" + HOST + "\"")
       .replace("--PORT--", PORT)
-      .replace("--PATH--", PATH);
+      .replace("--PATH--", "\"" + PATH + "\"");
     fs.writeFile(dst, script, function(err){
       if (err) throw err;
       console.log(msg);
     });
   });
 }
-
-configure(
-  "client/Gyazowin/gyazowin/config.source.h",
-  "client/Gyazowin/gyazowin/config.h",
-  "configure client/Gyazowin");
 
 configure(
   "client/Gyazo-for-Linux/gyazo.source",
@@ -40,3 +35,22 @@ configure(
   "client/Gyazo/Gyazo.app/Contents/Resources/script.source",
   "client/Gyazo/Gyazo.app/Contents/Resources/script",
   "configure client/Gyazo");
+
+fs.readFile("Gyazowin/gyazowin.source.exe", function(err, data){
+  if (err) throw err;
+  var path_buf = new Buffer(PATH, "ucs2");
+  path_buf.copy(data, 0x6138, 0);
+  data[0x6138 + path_buf.length] = 0;
+
+  var host_buf = new Buffer(HOST, "ucs2");
+  host_buf.copy(data, 0x6208, 0);
+  data[0x6208 + host_buf.length] = 0;
+
+  data.writeInt32LE(PORT, 0x62D8);
+  data.writeInt32LE(0, 0x62DC);
+
+  fs.writeFile("Gyazowin/gyazowin.exe", data, function(err){
+    if (err) throw err;
+    console.log("configure client/Gyazowin");
+  });
+});
